6ba8f0477f28541558f7dc39ac356893685668b9
techstackmedia <osagienoah04@gmail.com>
2022-01-13 17:54:20 +0100

    commit

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,24 +1,28 @@
-// const Joi = require("@hapi/joi");
+const bcrypt = require("bcryptjs");
 const User = require("../model/User");
 // const userValidation = require("../validation/validation");
 
 const { signUpValidation } = require("../validation/validation");
 
 const postSignup = async (req, res) => {
-  // const { error } = Joi.validate(req.body, schema);
-  // if (error) {
-  //   return res.status(400).send(error.details[0].message);
-  // }
   const { error } = signUpValidation(req.body);
   if (error) {
     return res.status(400).send(error.details[0].message);
   }
 
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) {
+    return res.status(400).send("Email already Exists");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassport = await bcrypt.hash(req.body.password, salt);
+
   const user = new User({
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassport,
     dob: req.body.dob,
     phoneNumber: req.body.phoneNumber,
   });
@@ -26,6 +30,7 @@ const postSignup = async (req, res) => {
   try {
     await user.save();
     return res.status(200).send(user);
+    // return res.status(200).send(user: user._id);
   } catch (err) {
     return res.status(400).send(err);
   }
--- model/User.js
+++ model/User.js
@@ -18,11 +18,12 @@ const userSchema = new mongoose.Schema(
     email: {
       type: String,
       required: true,
-      unique: true,
+      // unique: true,
     },
     phoneNumber: {
-      type: Number,
+      type: String,
       required: true,
+      // unique: true,
     },
     password: {
       type: String,
